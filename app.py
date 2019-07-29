337226750ee60b66d24b4102032e8bf30198eaa5
Xin <xinwen@mit.edu>
2019-07-29 22:17:51 +0300

    finished flask intro lab

--- app.py
+++ app.py
@@ -3,7 +3,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def home_page():
-    return "Hello World"
+    best = ["noodle", "ice cream", "matcha"]
+    worst = ["artificial cherry", "powder sugar"]
+    return render_template("index.html", fav=best, least_fav=worst, opposite_day=True)
 
 if __name__ == '__main__':
    app.run(debug = True)
